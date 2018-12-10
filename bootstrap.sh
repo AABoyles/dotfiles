b5e902ec40349c1c6ae9f48bd38f2a89e05b20fc
Tony Boyles <Anthony@Boyles.cc>
2018-12-10 13:34:04 -0500

    Rewrote bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,34 +1,13 @@
 #!/usr/bin/env bash
 
-# Preinstallations:
-
-## Atom
-sudo add-apt-repository ppa:webupd8team/atom
-
-## R
-gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E084DAB9
-gpg -a --export E084DAB9 | sudo apt-key add -
-echo "deb https://cran.rstudio.com/bin/linux/ubuntu `lsb_release -cs`/" | sudo tee -a /etc/apt/sources.list
-
-## Node
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-
-## Rodeo
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 33D40BC6
-sudo add-apt-repository "deb http://rodeo-deb.yhat.com/ rodeo main"
-
-## Spotify
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
-echo "deb http://repository.spotify.com stable non-free" | sudo tee -a /etc/apt/sources.list
-
-# Installations:
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y --install-recommends vim git curl build-essential \
-  r-base r-base-dev libssl-dev python3-dev python3-pip rodeo xsel atom \
-  nodejs libxml2-dev jq libcupti-dev linux-headers-$(uname -r) \
-  spotify-client wine-stable libcurl4-openssl-dev tesseract-ocr \
-  imagemagick scrot zsh
+## Core Libs
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y --install-recommends \
+  linux-headers-$(uname -r) software-properties-common \
+  build-essential curl libssl-dev vim git \
+  xsel libxml2-dev jq \
+  wine-stable libcurl4-openssl-dev \
+  tesseract-ocr imagemagick scrot zsh conky-all lm-sensors
 
 ## Now, let's get those dotfiles...
 if [ ! -d ~/Projects ]; then
@@ -38,41 +17,61 @@ fi
 if [ ! -d ~/Projects/dotfiles ]; then
   cd ~/Projects/
   git clone https://github.com/AABoyles/dotfiles.git
-  rm ~/.bash_aliases ~/.bash_exports ~/.bash_profile ~/.bashrc ~/.zshrc
-  ln -s ~/Projects/dotfiles/.bash* ~
-  ln -s ~/Projects/dotfiles/.zsh* ~
+  # rm ~/.bash_aliases ~/.bash_exports ~/.bash_profile ~/.bashrc ~/.zshrc
+  ln -s ~/Projects/dotfiles/.* ~
+  # ln -s ~/Projects/dotfiles/.zsh* ~
 fi
 
-# Non-Apt Installations:
-
-## Oh My zsh
-curl -L http://install.ohmyz.sh | sh
-
-## (And Syntax highlighting thereto)
-cd ~/.oh-my-zsh && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-
-## Node Packages
-sudo npm i -g npm yarn http-server
+## Python 3
+sudo apt install -y python3 python3-dev python3-pip jupyter-core
 
 ## Python Packages
-python3 -m pip install --upgrade pip
+sudo python3 -m pip install --upgrade pip
 sudo pip3 install thefuck csvkit numpy pandas sklearn matplotlib \
-  plotnine awscli aws-shell spacy howdoi jupyter jupyterlab
-sudo jupyter serverextension enable --py jupyterlab --sys-prefix
+  plotnine awscli aws-shell spacy howdoi
 
-## R Packages
+## R
+gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E084DAB9
+gpg -a --export E084DAB9 | sudo apt-key add -
+echo "deb https://cran.rstudio.com/bin/linux/ubuntu `lsb_release -cs`/" | sudo tee -a /etc/apt/sources.list
+sudo apt install -y --install-recommends r-base r-base-dev
 
+### R Packages
+/usr/bin/Rscript -e "install.packages('tidyverse')"
 
-## XGBoost
-git clone --recursive https://github.com/dmlc/xgboost ~/Packages/xgboost
-cd ~/Packages/xgboost
-make -j4
-cd python-package
-sudo python3 setup.py install
-cd ~
+## Node
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+### Node Packages
+sudo npm i -g http-server
+
+### [Atom](https://atom.io/)
+cd ~/Downloads/
+curl https://atom.io/download/deb
+sudo dpkg -i atom-amd64.deb
+
+## [Rust](https://www.rust-lang.org/)
+sudo apt install -y rustc cargo
+
+### [XSV](https://github.com/BurntSushi/xsv#readme)
+sudo cargo install xsv
+
+## [Haskell]()
+sudo apt-get install -y haskell-platform
+sudo add-apt-repository -y ppa:hvr/ghc
+sudo apt-get update
+sudo apt-get install -y cabal-install-2.4 ghc-8.4.4
+sudo stack upgrade --binary-only
+
+## Oh My zsh
+# curl -L http://install.ohmyz.sh | sh
+
+## (And Syntax highlighting thereto)
+# cd ~/.oh-my-zsh && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
 # Now, set the default terminal to be zsh
-sudo chsh -s `which zsh`
+# sudo chsh -s `which zsh`
 
 # Reset so the System Works the way you expect it to.
 sudo shutdown -r now
